76933acd209d34668f473b65b7ddd78e4efde006
irisida <20445648+irisida@users.noreply.github.com>
2021-01-16 16:54:56 +0000

    added recursion example

---
+++ chapter04/FactorialsWithRecursion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FactorialsWithRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 15; i++)
+            {
+                Console.WriteLine($"{i}! = {Factorial(i):N0}");
+            }
+        }
+
+        /// <summary>
+        /// Expects an int to be passed in and it will return the factorial value using a recursive implementation.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>An int as the final result of calculating the factorial</returns>
+        static int Factorial(int number)
+        {
+            if (number < 1)
+            {
+                return 0;
+            }
+            else if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                // uncomment to see recursive calls in action
+                // Console.WriteLine($"calling {number} * Fractorial({number - 1})");
+                return number * Factorial(number - 1);
+            }
+        }
+    }
+}
