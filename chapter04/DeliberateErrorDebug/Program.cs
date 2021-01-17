e5a55a11c2fd12dcbf90ee17a2493a1083953323
irisida <20445648+irisida@users.noreply.github.com>
2021-01-17 12:31:53 +0000

    added debug deliberate error sample, intriduces a tracefile

---
+++ chapter04/DeliberateErrorDebug/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+
+namespace DeliberateErrorDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
+            Trace.AutoFlush = true;
+
+            double a = 4.5;
+            Trace.WriteLine($"Variable assignment a = {a}");
+
+            double b = 2.5;
+            Trace.WriteLine($"Variable assignment b = {b}");
+
+            double answer = Add(a, b);
+            Trace.WriteLine($"calculation expects {a + b} function returned {answer}");
+
+            Console.WriteLine($"{a} + {b} = {answer}");
+            Console.ReadLine();
+        }
+
+        static double Add(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
