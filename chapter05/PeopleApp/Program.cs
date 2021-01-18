13cc3a5869db2c5210495cb07c970aa3f508c88f
irisida <20445648+irisida@users.noreply.github.com>
2021-01-18 17:35:45 +0000

    added OOP with output formatting changes

--- chapter05/PeopleApp/Program.cs
+++ chapter05/PeopleApp/Program.cs
@@ -10,7 +10,20 @@ namespace PeopleApp
             var person = new Person();
             person.Name = "Swiss Tony";
             person.DateOfBirth = new DateTime(1973, 12, 30);
-            Console.WriteLine(person.ToString());
+            Console.WriteLine(
+                format: "{0} was born on {1:dddd, d, MMMM, yyyy}",
+                arg0: person.Name,
+                arg1: person.DateOfBirth
+            );
+
+            var p2 = new Person();
+            p2.Name = "Jac the hat";
+            p2.DateOfBirth = new DateTime(1951, 1, 1);
+            Console.WriteLine(
+                format: "{0} was born on {1:dd, MMM, yyyy}",
+                arg0: p2.Name,
+                arg1: p2.DateOfBirth
+            );
         }
     }
 }
