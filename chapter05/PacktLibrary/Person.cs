ffab06370b30c4f4c272ddf9824db62541c1332e
irisida <20445648+irisida@users.noreply.github.com>
2021-01-18 11:33:06 +0000

    added class lib demo

---
+++ chapter05/PacktLibrary/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace Packt.Shared
+{
+    public class Person : System.Object
+    {
+        public string Name;
+        public DateTime DateOfBirth;
+
+    }
+}
---
+++ chapter05/PeopleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Packt.Shared;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "Swiss Tony";
+            person.DateOfBirth = new DateTime(1973, 12, 30);
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
