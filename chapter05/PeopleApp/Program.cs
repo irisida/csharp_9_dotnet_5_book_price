a66266505d520b89c436bbf4cd5c6a5c226e15b3
irisida <20445648+irisida@users.noreply.github.com>
2021-01-18 21:16:06 +0000

    added a byte based enum and possibility to build option combo

--- chapter05/PeopleApp/Program.cs
+++ chapter05/PeopleApp/Program.cs
@@ -7,23 +7,32 @@ namespace PeopleApp
     {
         static void Main(string[] args)
         {
-            var person = new Person();
-            person.Name = "Swiss Tony";
-            person.DateOfBirth = new DateTime(1973, 12, 30);
+            var p1 = new Person();
+            p1.Name = "Swiss Tony";
+            p1.DateOfBirth = new DateTime(1973, 12, 30);
+            p1.BucketList = WondersOfTheAncientWorld.GreatPyramidOfGiza |
+                WondersOfTheAncientWorld.LighthouseOfAlexandria;
+
+
+            // using formatted strings
             Console.WriteLine(
-                format: "{0} was born on {1:dddd, d, MMMM, yyyy}",
-                arg0: person.Name,
-                arg1: person.DateOfBirth
+                format: "{0} was born on {1:dddd, d, MMMM, yyyy}.",
+                arg0: p1.Name,
+                arg1: p1.DateOfBirth
             );
+            Console.WriteLine($"{p1.Name}'s bucket list is: {p1.BucketList}");
 
             var p2 = new Person();
             p2.Name = "Jac the hat";
             p2.DateOfBirth = new DateTime(1951, 1, 1);
+            p2.BucketList = WondersOfTheAncientWorld.MausoleumAtHalicarnassus | WondersOfTheAncientWorld.TempleOfArtemisAtEphesus;
+
             Console.WriteLine(
-                format: "{0} was born on {1:dd, MMM, yyyy}",
+                format: "{0} was born on {1:dd, MMM, yyyy}.",
                 arg0: p2.Name,
                 arg1: p2.DateOfBirth
             );
+            Console.WriteLine($"{p2.Name}'s bucket list is: {p2.BucketList}");
         }
     }
 }
