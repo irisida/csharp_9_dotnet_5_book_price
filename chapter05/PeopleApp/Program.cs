85134c8f76e66ca5042b7292db59abe0d4b728cb
irisida <20445648+irisida@users.noreply.github.com>
2021-01-20 16:41:40 +0000

    added sample record

--- chapter05/PeopleApp/Program.cs
+++ chapter05/PeopleApp/Program.cs
@@ -49,6 +49,42 @@ namespace PeopleApp
                 arg0: jonesAccount.AccountName,
                 arg1: jonesAccount.Balance * BankAccount.InterestRate
             );
+
+            // c# 8 features
+            object[] passengers = {
+                new FirstClassPassenger {AirMiles = 1_419 },
+                new FirstClassPassenger {AirMiles = 16_500 },
+                new BusinessClassPassenger(),
+                new CoachClassPassenger { CarryOnKG = 25.7},
+                new CoachClassPassenger { CarryOnKG = 0 },
+            };
+
+            foreach (object passenger in passengers)
+            {
+                // c# 8 specific syntax
+                decimal flightCost = passenger switch
+                {
+                    FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
+                    FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
+                    FirstClassPassenger _ => 2000M,
+                    BusinessClassPassenger _ => 1000M,
+                    CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+                    CoachClassPassenger _ => 650M,
+                    _ => 800M
+                };
+
+                Console.WriteLine($"Flight costs {flightCost:C} for {passenger}");
+
+            }
+
+            // immutability concept using class with init properties
+            var SwissTony = new ImmutablePerson
+            {
+                FirstName = "Swiss Tony",
+                LastName = "Foshizzle"
+            };
+
+
         }
 
         static void DisplayValues(Person p)
